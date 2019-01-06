65f93087c011c23a411c8c4d7ff5db17fc352f33
sub <elhayr1@gmail.com>
2019-01-06 11:25:28 +0200

    updating installtion

--- mobilican/setup/setup.sh
+++ mobilican/setup/setup.sh
@@ -12,18 +12,6 @@ set -eb
 # welcome screen
 whiptail --textbox robotican.txt_img 20 75
 
-# robot selection menu
-#lizi_2="Lizi_2"
-#armadillo_1="armadillo_1"
-#komodo_2="komodo_2"
-#armadillo_2="armadillo_2"
-#ROBOT_SELECTION=$(whiptail --title "Choose your robot. Press space to select" --radiolist "Choose" 20 50 15 \
-#"$lizi_2" "Lizi 2 robot" ON \
-#"$komodo_2" "Komodo 2 robot" OFF \
-#"$armadillo_1" "Armadillo 1 robot" OFF \
-#"$armadillo_2" "Armadillo 2 robot" OFF \
-#3>&1 1>&2 2>&3)
-
 # get user password for sudo use
 psw=$(whiptail --title "Password Box" --passwordbox "Enter your password and choose Ok to continue." 10 60 3>&1 1>&2 2>&3)
 # check user password 
@@ -72,121 +60,129 @@ update_progress() {
     update_progress 19 "Updating packages... Done."
 
     # installing ros packages
-    update_progress 19 "Installing ROS packages..."
-    update_progress 19 "Installing ROS packages... installing controller-manager..."
+    update_progress 19 "Installing packages..."
+    update_progress 19 "Installing packages... installing controller-manager..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-controller-manager 
-    update_progress 21 "Installing ROS packages... installing controller-manager... Done"
-    update_progress 21 "Installing ROS packages... installing control-toolbox..."
+    update_progress 21 "Installing packages... installing controller-manager... Done"
+    update_progress 21 "Installing packages... installing control-toolbox..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-control-toolbox 
-    update_progress 23 "Installing ROS packages... installing control-toolbox... Done"
-    update_progress 23 "Installing ROS packages... installing control-toolbox..."
+    update_progress 23 "Installing packages... installing control-toolbox... Done"
+    update_progress 23 "Installing packages... installing control-toolbox..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-transmission-interface 
-    update_progress 25 "Installing ROS packages... installing transmission-interface... Done"
-    update_progress 25 "Installing ROS packages... installing joint-limits-interface..."
+    update_progress 25 "Installing packages... installing transmission-interface... Done"
+    update_progress 25 "Installing packages... installing joint-limits-interface..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-joint-limits-interface 
-    update_progress 26 "Installing ROS packages... installing joint-limits-interface... Done"
-    update_progress 26 "Installing ROS packages... installing ros-controllers..."
+    update_progress 26 "Installing packages... installing joint-limits-interface... Done"
+    update_progress 26 "Installing packages... installing controllers..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-ros-controllers 
-    update_progress 29 "Installing ROS packages... installing ros-controllers... Done"
-    update_progress 29 "Installing ROS packages... installing ros-control..."
+    update_progress 29 "Installing packages... installing controllers... Done"
+    update_progress 29 "Installing packages... installing control..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-ros-control 
-    update_progress 30 "Installing ROS packages... installing ros-control... Done"
-    update_progress 30 "Installing ROS packages... installing move-base..."
+    update_progress 30 "Installing packages... installing control... Done"
+    update_progress 30 "Installing packages... installing move-base..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-move-base 
-    update_progress 31 "Installing ROS packages... installing move-base... Done"
-    update_progress 31 "Installing ROS packages... installing navigation..."
+    update_progress 31 "Installing packages... installing move-base... Done"
+    update_progress 31 "Installing packages... installing navigation..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-navigation 
-    update_progress 33 "Installing ROS packages... installing navigation... Done"
-    update_progress 33 "Installing ROS packages... installing hector-slam..."
+    update_progress 33 "Installing packages... installing navigation... Done"
+    update_progress 33 "Installing packages... installing hector-slam..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-hector-slam 
-    update_progress 34 "Installing ROS packages... installing hector-slam... Done"
-    update_progress 34 "Installing ROS packages... installing gmapping..."
+    update_progress 34 "Installing packages... installing hector-slam... Done"
+    update_progress 34 "Installing packages... installing gmapping..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-gmapping 
-    update_progress 35 "Installing ROS packages... installing gmapping... Done"
-    update_progress 35 "Installing ROS packages... installing pid..."
+    update_progress 35 "Installing packages... installing gmapping... Done"
+    update_progress 35 "Installing packages... installing pid..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-pid 
-    update_progress 36 "Installing ROS packages... installing pid... Done"
-    update_progress 36 "Installing ROS packages... installing ar-track-alvar..."
+    update_progress 36 "Installing packages... installing pid... Done"
+    update_progress 36 "Installing packages... installing ar-track-alvar..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-ar-track-alvar 
-    update_progress 37 "Installing ROS packages... installing ar-track-alvar... Done"
-    update_progress 37 "Installing ROS packages... installing serial..."
+    update_progress 37 "Installing packages... installing ar-track-alvar... Done"
+    update_progress 37 "Installing packages... installing serial..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-serial 
-    update_progress 38 "Installing ROS packages... installing serial... Done"
-    update_progress 38 "Installing ROS packages... installing robot-localization..."
+    update_progress 38 "Installing packages... installing serial... Done"
+    update_progress 38 "Installing packages... installing robot-localization..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-robot-localization 
-    update_progress 39 "Installing ROS packages... installing robot-localization... Done"
-    update_progress 39 "Installing ROS packages... installing trac-ik..."
+    update_progress 39 "Installing packages... installing robot-localization... Done"
+    update_progress 39 "Installing packages... installing trac-ik..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-trac-ik 
-    update_progress 40 "Installing ROS packages... installing trac-ik... Done"
-    update_progress 40 "Installing ROS packages... installing moveit-kinematics..."
+    update_progress 40 "Installing packages... installing trac-ik... Done"
+    update_progress 40 "Installing packages... installing moveit-kinematics..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-moveit-kinematics  
-    update_progress 41 "Installing ROS packages... installing moveit-kinematics... Done"
-    update_progress 41 "Installing ROS packages... installing urg-node..."
+    update_progress 41 "Installing packages... installing moveit-kinematics... Done"
+    update_progress 41 "Installing packages... installing urg-node..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-urg-node 
-    update_progress 42 "Installing ROS packages... installing urg-node... Done"
-    update_progress 42 "Installing ROS packages... installing usb-cam..."
+    update_progress 42 "Installing packages... installing urg-node... Done"
+    update_progress 42 "Installing packages... installing usb-cam..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-usb-cam 
-    update_progress 43 "Installing ROS packages... installing usb-cam... Done"
-    update_progress 43 "Installing ROS packages... installing rqt-robot-monitor..."
+    update_progress 43 "Installing packages... installing usb-cam... Done"
+    update_progress 43 "Installing packages... installing rqt-robot-monitor..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-rqt-robot-monitor 
-    update_progress 44 "Installing ROS packages... installing controller rqt-robot-monitor... Done"
-    update_progress 44 "Installing ROS packages... installing control hector-gazebo-plugins..."
+    update_progress 44 "Installing packages... installing rqt-robot-monitor... Done"
+    update_progress 44 "Installing packages... installing gazebo-ros-control..."
+    sudo -S <<< $psw apt-get -y install ros-kinetic-gazebo-ros-control
+    update_progress 45 "Installing packages... installing gazebo-ros-control... Done"
+    update_progress 45 "Installing packages... installing kinetic-moveit..."
+    sudo -S <<< $psw apt-get -y install ros-kinetic-moveit
+    update_progress 46 "Installing packages... installing kinetic-moveit... Done"
+    update_progress 46 "Installing packages... installing moveit-ros-planning..."
+    sudo -S <<< $psw apt-get -y install ros-kinetic-moveit-ros-planning
+    update_progress 47 "Installing packages... installing moveit-ros-planning... Done"
+    update_progress 47 "Installing packages... installing moveit-ros-planning-interface..."
+    sudo -S <<< $psw apt-get -y install ros-kinetic-moveit-ros-planning-interface
+    update_progress 48 "Installing packages... installing moveit-ros-planning-interface... Done"
+    update_progress 48 "Installing packages... installing twist-mux..."
+    sudo -S <<< $psw apt-get -y install ros-kinetic-twist-mux
+    update_progress 49 "Installing packages... installing twist-mux... Done"
+    update_progress 49 "Installing packages... installing joy..."
+    sudo -S <<< $psw apt-get -y install ros-kinetic-joy
+    update_progress 50 "Installing packages... installing joy... Done"
+    update_progress 50 "Installing packages... installing joystick..."
+    sudo -S <<< $psw apt-get -y install joystick
+    update_progress 51 "Installing packages... installing joystick... Done"
+    update_progress 51 "Installing packages... installing jstest-gtk..."
+    sudo -S <<< $psw apt-get -y install jstest-gtk
+    update_progress 52 "Installing packages... installing jstest-gtk... Done"
+    update_progress 52 "Installing packages... installing hector-gazebo-plugins..."
     sudo -S <<< $psw apt-get -y install ros-kinetic-hector-gazebo-plugins 
-    update_progress 53 "Installing ROS packages... installing hector-gazebo-plugins... Done"
-    update_progress 53 "Installing ROS packages... Done."
+    update_progress 53 "Installing packages... installing hector-gazebo-plugins... Done"
+    update_progress 53 "Installing packages... Done."
 
     #installing 3rd party packages
-    update_progress 53 "Installing ROS packages... installing espeak..."
+    update_progress 53 "Installing packages... installing espeak..."
     sudo -S <<< $psw apt-get -y install espeak espeak-data libespeak-dev
-    update_progress 54 "Installing ROS packages... installing espeak... Done"
+    update_progress 54 "Installing packages... installing espeak... Done"
 
-    update_progress 54 "Installing 3rd party packages: diff_slip_controller..."
-    DIFF_SLIP_CONTROLLER_V="1.0.0"
-    wget https://github.com/robotican/diff_drive_slip_controller/archive/V"$DIFF_SLIP_CONTROLLER_V".tar.gz
-    tar -xvzf V"$DIFF_SLIP_CONTROLLER_V".tar.gz
-    rm V"$DIFF_SLIP_CONTROLLER_V".tar.gz
-    update_progress 55 "Installing 3rd party packages: diff_slip_controller... Done"
-
-    update_progress 55 "Installing 3rd party packages: ric_interface_ros..."
+    update_progress 54 "Installing packages: ric_interface_ros..."
     RIC_INTERFACE_ROS_V="1.0.6"
     wget https://github.com/robotican/ric_interface_ros/archive/V"$RIC_INTERFACE_ROS_V".tar.gz
     tar -xvzf V"$RIC_INTERFACE_ROS_V".tar.gz
     rm V"$RIC_INTERFACE_ROS_V".tar.gz
-    update_progress 57 "Installing 3rd party packages: ric_interface_ros... Done"
+    update_progress 57 "Installing packages: ric_interface_ros... Done"
 
-    update_progress 57 "Installing 3rd party packages: mobilican_macros..."
-    MOBILICAN_MACROS_V="1.0.0"
-    wget https://github.com/robotican/mobilican_macros/archive/V"$MOBILICAN_MACROS_V".tar.gz
-    tar -xvzf V"$MOBILICAN_MACROS_V".tar.gz
-    rm V"$MOBILICAN_MACROS_V".tar.gz
-    update_progress 58 "Installing 3rd party packages: mobilican_macros... Done"
+    # --- RESERVE PLACE FOR PACKAGE INSTALLATION ---
 
-
-    update_progress 58 "Installing 3rd party packages: lpf_ros..."
+    update_progress 57 "Installing packages: lpf_ros..."
     LPF_ROS_V="1.0.1"
     wget https://github.com/elhayra/lpf_ros/archive/V"$LPF_ROS_V".tar.gz
     tar -xvzf V"$LPF_ROS_V".tar.gz
     rm V"$LPF_ROS_V".tar.gz
-    update_progress 60 "Installing 3rd party packages: lpf_ros... Done"
+    update_progress 60 "Installing packages: lpf_ros... Done"
 
-
-    update_progress 60 "Installing 3rd party packages: espeak_ros"
+    update_progress 60 "Installing packages: espeak_ros"
     ESPEAK_ROS_V="1.0.2"
     wget https://github.com/robotican/espeak_ros/archive/V"$ESPEAK_ROS_V".tar.gz
     tar -xvzf V"$ESPEAK_ROS_V".tar.gz
     rm V"$ESPEAK_ROS_V".tar.gz
-    update_progress 63 "Installing 3rd party packages: espeak_ros... Done"
-
+    update_progress 63 "Installing packages: espeak_ros... Done"
 
     #install ric_interface deb
-    update_progress 71 "Installing 3rd party packages: ric_interface..."
+    update_progress 63 "Installing packages: ric_interface..."
     cd $CATKIN_WS_SRC/ric_interface_ros-$RIC_INTERFACE_ROS_V/ric_interface_deb/
     sudo -S <<< $psw dpkg -i ric-interface.deb
-    update_progress 74 "Installing 3rd party packages: ric_interface... Done"
-
+    update_progress 74 "Installing packages: ric_interface... Done"
 
     # realsense depth camera 
-    update_progress 74 "Installing 3rd party packages: realsense..."
+    update_progress 74 "Installing packages: realsense..."
     cd $CATKIN_WS_SRC/
     wget https://github.com/intel-ros/realsense/archive/2.0.3.tar.gz
     tar -xvzf 2.0.3.tar.gz
@@ -199,9 +195,9 @@ update_progress() {
     mkdir build && cd build               
     cmake ../  
     sudo -S <<< $psw make uninstall && make clean && make -j8 && sudo make install
-    update_progress 83 "Installing 3rd party packages: realsense... Done"
+    update_progress 83 "Installing packages: realsense... Done"
 
-    update_progress 83 "Installing 3rd party packages... Done."
+    update_progress 83 "Installing packages... Done."
 
     # installing usb rules
     update_progress 83 "Installing USB rules..."
@@ -229,9 +225,7 @@ update_progress() {
 } | whiptail --title "Installing Mobilican" --gauge "Preparing..." 6 60 0
 # end of whiptail progressbar command
 
-# finish box
-whiptail --textbox "Please restart your PC to complete installation process" 20 75
-
-
-
+# finish and reboot
+whiptail --title "Installation Completed" --msgbox "After hitting OK, PC will restart to complete installation." 8 78
+sudo -S <<< $psw reboot
 
