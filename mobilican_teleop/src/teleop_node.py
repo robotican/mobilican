43a72cdd50a75483290348ad470175b5aedfb36a
sub <elhayr1@gmail.com>
2019-02-03 15:45:33 +0200

    adding more options to teleop node

--- mobilican_teleop/src/teleop_node.py
+++ mobilican_teleop/src/teleop_node.py
@@ -4,11 +4,17 @@ import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 
-axis_linear = 3
-axis_angular = 2
+angular_axis = 2
+angular_reverse = False
+angular_clamp = 0
+angular_multiplier = 1
+
+linear_axis = 3
+linear_reverse = False
+linear_clamp = 0
+linear_multiplier = 1
+
 deadman_button = 6
-reverse_linear = False
-reverse_angular = False
 twist_topic = "mobile_base_controller/cmd_vel"
 
 def joyCallback(msg):
@@ -17,17 +23,25 @@ def joyCallback(msg):
 
     twist_msg = Twist()
     if msg.buttons[deadman_button]:
-        twist_msg.linear.x = msg.axes[axis_linear]
-        if reverse_linear:
+        twist_msg.linear.x = msg.axes[linear_axis] * linear_multiplier
+        if linear_reverse:
             twist_msg.linear.x *= -1
-
-        twist_msg.angular.z = msg.axes[axis_angular]
-        if reverse_angular:
+        if twist_msg.linear.x > linear_clamp:
+            twist_msg.linear.x = linear_clamp
+        if twist_msg.linear.x < -linear_clamp:
+            twist_msg.linear.x = -linear_clamp
+        
+        twist_msg.angular.z = msg.axes[angular_axis] * angular_multiplier
+        if angular_reverse:
             twist_msg.angular.z *= -1
+        if twist_msg.linear.z > angular_clamp:
+            twist_msg.angular.z = angular_clamp
+        if twist_msg.linear.z < -angular_clamp:
+            twist_msg.linear.z = -angular_clamp
+
     else:
         twist_msg.linear.x = 0
         twist_msg.angular.z = 0
-        #TODO: send emergency stop to roboteq
 
     twist_pub.publish(twist_msg)
 
@@ -37,11 +51,17 @@ if __name__ == '__main__':
 
     rospy.init_node('teleop_node', anonymous=False)
 
-    rospy.get_param('~axis_linear', axis_linear)
-    rospy.get_param('~axis_angular', axis_angular)
+    rospy.get_param('~angular_axis', angular_axis)
+    rospy.get_param('~angular_reverse', angular_reverse)
+    rospy.get_param('~angular_clamp', angular_clamp)
+    rospy.get_param('~angular_multiplier', angular_multiplier)
+
+    rospy.get_param('~linear_axis', linear_axis)
+    rospy.get_param('~linear_reverse', linear_reverse)
+    rospy.get_param('~linear_clamp', linear_clamp)
+    rospy.get_param('~linear_multiplier', linear_multiplier)
+
     rospy.get_param('~deadman_button', deadman_button)
-    rospy.get_param('~reverse_angular', reverse_angular)
-    rospy.get_param('~reverse_linear', reverse_linear)
     rospy.get_param('~twist_topic', twist_topic)
 
     rospy.Subscriber("/joy", Joy, joyCallback)
